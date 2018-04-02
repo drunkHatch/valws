355657e5c6d7e417ceceae23a22ee0b24a4ea1ba
drunkHatch <32227266+drunkHatch@users.noreply.github.com>
2018-04-02 16:32:37 -0600

    Add files via upload

---
+++ valws379.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFERSIZE    1
+
+int main(int argc, char **argv){
+    unsigned char     buffer[BUFFERSIZE];
+    FILE                         *instream;
+    int                            bytes_read=0;
+    int                            buffer_size=0;
+
+
+    buffer_size=sizeof(unsigned char)*BUFFERSIZE;
+    /* open stdin for reading */
+    instream=fopen("/dev/stdin","r");
+
+    /* did it open? */
+    if(instream!=NULL){
+        /* read from stdin until it's end */
+        while((bytes_read=fread(&buffer, buffer_size, 1, instream))==buffer_size){
+            fprintf(stdout, "%c", buffer[0]);
+        }
+    }
+    /* if any error occured, exit with an error message */
+    else{
+        fprintf(stderr, "ERROR opening stdin. aborting.\n");
+        exit(1);
+    }
+
+    return(0);
+}
+
+typedef struct{
+  long long int memory_address;
+  long long int page_number;
+  long long int offset;
+  M_REFERENCE *prev;
+  M_REFERENCE *next;
+  
+} M_REFERENCE;
