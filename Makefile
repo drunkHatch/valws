2bd2ae478ff804007cbc98463efed537c99817e0
taijie <taijie@ualberta.ca>
2018-04-02 16:34:02 -0600

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,6 @@
+all:
+	gcc heapsort.c -o test -Wall
+	gcc valws379.c -o valws379 -Wall 
+	valgrind --tool=lackey --trace-mem=yes ./test 2>&1 | ./valws379 16 5
+clean:
+	rm valws379 test
--- valws379.c
+++ valws379.c
@@ -1,34 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define BUFFERSIZE    1
-
-int main(int argc, char **argv){
-    unsigned char     buffer[BUFFERSIZE];
-    FILE                         *instream;
-    int                            bytes_read=0;
-    int                            buffer_size=0;
-
-
-    buffer_size=sizeof(unsigned char)*BUFFERSIZE;
-    /* open stdin for reading */
-    instream=fopen("/dev/stdin","r");
-
-    /* did it open? */
-    if(instream!=NULL){
-        /* read from stdin until it's end */
-        while((bytes_read=fread(&buffer, buffer_size, 1, instream))==buffer_size){
-            fprintf(stdout, "%c", buffer[0]);
-        }
-    }
-    /* if any error occured, exit with an error message */
-    else{
-        fprintf(stderr, "ERROR opening stdin. aborting.\n");
-        exit(1);
-    }
-
-    return(0);
-}
+#include <string.h>
+#define BUFFERSIZE 1
 
 typedef struct{
   long long int memory_address;
@@ -38,3 +11,62 @@ typedef struct{
   M_REFERENCE *next;
   
 } M_REFERENCE;
+
+
+
+long long int extract_number(char * raw_data);
+
+int main(){
+    char buffer[14];
+    FILE          *instream;
+    int           bytes_read = 0;
+    int           buffer_size = 0;
+    char          address[9];
+    long long int           dec_num;
+
+    buffer_size = sizeof(unsigned char)*BUFFERSIZE;
+
+    //open stdin for reading
+    instream = fopen("/dev/stdin","r");
+
+    if(instream != NULL){
+        //read from stdin untill it's end
+        while(fgets(buffer,13,instream)){
+            int buffer_length;
+
+            buffer_length = strlen(buffer);
+            if (buffer_length > 4){
+                if (buffer[0] == ' '){
+                    if (buffer[1] == 'S' || buffer[1] == 'M' ||  buffer[1] == 'L'){
+                        dec_num = extract_number(buffer);
+                        printf("%lli\n",dec_num,dec_num);
+                    }
+                }else if(buffer[0] == 'I'){
+                    dec_num = extract_number(buffer);
+                    printf("%lli\n",dec_num,dec_num);
+                }   
+            }
+            //fprintf(stdout,"%s\n",buffer);
+        }
+    }
+
+}
+
+
+long long int extract_number(char * raw_data){
+    char extracted_hex[9];
+    long long int extracted_dec;
+    int length;
+    
+    length = strlen(raw_data);
+    for(int i=3; i<length; i++){
+        if(raw_data[i] != ','){
+            extracted_hex[i-3] = raw_data[i];
+        }else{
+            extracted_hex[i-3] = '\0';
+        }
+    }
+    sscanf(extracted_hex, "%llX", &extracted_dec);
+
+    return extracted_dec;
+}
