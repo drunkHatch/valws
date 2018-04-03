5cd2d7c406d2058761e3fc8225a89edd498f96ae
taijie <taijie@ualberta.ca>
2018-04-02 18:20:47 -0600

    Update valws379.c

--- valws379.c
+++ valws379.c
@@ -3,26 +3,26 @@
 #include <string.h>
 #define BUFFERSIZE 1
 
-typedef struct{
-  long long int memory_address;`    `
-  long long int page_number;
-  long long int offset;
+typedef struct {
+  long long int memory_address;
+  long long int page_number;   // divided by 16
   void *prev;
   void *next;
 
 } M_REFERENCE;
 
-
-
 long long int extract_number(char * raw_data);
 
-int main(){
+int main(int argc, char *argv[]){
     char buffer[14];
+    int pagesize = atoi(argv[1]);
+    int wsize = atoi(argv[2]);
     FILE          *instream;
     int           bytes_read = 0;
     int           buffer_size = 0;
     char          address[9];
     long long int           dec_num;
+    M_REFERENCE   node;
 
     buffer_size = sizeof(unsigned char)*BUFFERSIZE;
 
@@ -39,11 +39,20 @@ int main(){
                 if (buffer[0] == ' '){
                     if (buffer[1] == 'S' || buffer[1] == 'M' ||  buffer[1] == 'L'){
                         dec_num = extract_number(buffer);
-                        printf("%lli\n",dec_num,dec_num);
+                        printf("%lli\n",dec_num/pagesize);
+                        //node.prev = node;
+                        node.memory_address = dec_num;
+                        node.page_number = dec_num/pagesize;
+
+                        
+                        
                     }
                 }else if(buffer[0] == 'I'){
                     dec_num = extract_number(buffer);
-                    printf("%lli\n",dec_num,dec_num);
+                    printf("%lli\n",dec_num/pagesize);
+                    node.memory_address = dec_num;
+                    node.page_number = dec_num/pagesize;
+                    
                 }
             }
             //fprintf(stdout,"%s\n",buffer);
@@ -57,7 +66,7 @@ long long int extract_number(char * raw_data){
     char extracted_hex[9];
     long long int extracted_dec;
     int length;
-
+    
     length = strlen(raw_data);
     for(int i=3; i<length; i++){
         if(raw_data[i] != ','){
@@ -66,7 +75,7 @@ long long int extract_number(char * raw_data){
             extracted_hex[i-3] = '\0';
         }
     }
-    sscanf(extracted_hex, "%llX", &extracted_dec);
+    sscanf(extracted_hex, "%X", &extracted_dec);
 
     return extracted_dec;
 }
