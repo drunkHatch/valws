f2e2f7d2ae1326662aacf08d6caa4fb16aa184e5
taijie <taijie@ualberta.ca>
2018-04-04 16:07:43 -0600

    Add files via upload

--- valws379.c
+++ valws379.c
@@ -1,72 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+
 #define BUFFERSIZE 1
 
-typedef struct {
-  long long int memory_address;
-  long long int page_number;   // divided by 16
-  void *prev;
-  void *next;
+struct Node{
+    long long int page_number;   // divided by 16
+    struct Node *prev;
+    struct Ndoe *next;
+};
 
-} M_REFERENCE;
+
+struct Window{
+    int amount;
+    struct Node *first;
+    struct Node *last;
+};
+
 
 long long int extract_number(char * raw_data);
 
 int main(int argc, char *argv[]){
-    char buffer[14];
-    int pagesize = atoi(argv[1]);
-    int wsize = atoi(argv[2]);
-    FILE          *instream;
-    int           bytes_read = 0;
-    int           buffer_size = 0;
-    char          address[9];
-    long long int           dec_num;
-    M_REFERENCE   node;
-
+    char            buffer[14];
+    int             pagesize;
+    int             wsize;
+    FILE            *instream;
+    int             bytes_read = 0;
+    int             buffer_size = 0;
+    char            address[9];
+    long long int   dec_num;
+    long long int   page_number;
+    struct Window   *window = malloc(sizeof(struct Window));
     buffer_size = sizeof(unsigned char)*BUFFERSIZE;
 
-    //open stdin for reading
-    instream = fopen("/dev/stdin","r");
-
-    if(instream != NULL){
+    //init window
+    window->first = NULL;
+    window->last = NULL;
+    if(argc == 3){                   //without [-i]
+        pagesize = atoi(argv[1]);
+        wsize = atoi(argv[2]);
+        //open stdin for reading
+        instream = fopen("/dev/stdin","r");
         //read from stdin untill it's end
         while(fgets(buffer,13,instream)){
             int buffer_length;
-
             buffer_length = strlen(buffer);
             if (buffer_length > 4){
                 if (buffer[0] == ' '){
                     if (buffer[1] == 'S' || buffer[1] == 'M' ||  buffer[1] == 'L'){
                         dec_num = extract_number(buffer);
                         printf("%lli\n",dec_num/pagesize);
-                        //node.prev = node;
-                        node.memory_address = dec_num;
-                        node.page_number = dec_num/pagesize;
 
-                        
-                        
+                        page_number = dec_num/pagesize;
+                        if (window->amount < wsize){
+                            addBack(window, page_number);
+                            window->amount = window->amount + 1;
+                        }else{
+                            leaveFront (window);
+                            addBack(window, page_number);
+                            //TODO:  compare in the window and get unique
+                        }
                     }
                 }else if(buffer[0] == 'I'){
-                    dec_num = extract_number(buffer);
-                    printf("%lli\n",dec_num/pagesize);
-                    node.memory_address = dec_num;
-                    node.page_number = dec_num/pagesize;
-                    
+                    if(buffer[1] == ' '){
+                        dec_num = extract_number(buffer);
+                        printf("%lli\n",dec_num/pagesize);
+
+                        page_number = dec_num/pagesize;
+                        if (window->amount < wsize){
+                            addBack(window, page_number);
+                            window->amount = window->amount + 1;
+                        }else{
+                            leaveFront (window);
+                            addBack(window, page_number);
+                            //TODO:  compare in the window and get unique
+                        }
+
+                    }
                 }
             }
             //fprintf(stdout,"%s\n",buffer);
         }
-    }
+        
 
+    }else{      // with [-i]
+        pagesize = atoi(argv[2]);
+        wsize = atoi(argv[3]);
+        //open stdin for reading
+        instream = fopen("/dev/stdin","r");
+        //read from stdin untill it's end
+        while(fgets(buffer,13,instream)){
+            int buffer_length;
+            buffer_length = strlen(buffer);
+            if (buffer_length > 4){
+                if (buffer[0] == ' '){
+                    if (buffer[1] == 'S' || buffer[1] == 'M' ||  buffer[1] == 'L'){
+                        dec_num = extract_number(buffer);
+                        printf("%lli\n",dec_num/pagesize);
+
+                        page_number = dec_num/pagesize;
+                        if (window->amount < wsize){
+                            addBack(window, page_number);
+                            window->amount = window->amount + 1;
+                        }else{
+                            leaveFront (window);
+                            addBack(window, page_number);
+                            //TODO:  compare in the window and get unique
+                        }
+
+                    }
+                }
+            }
+            //fprintf(stdout,"%s\n",buffer);
+        }
+        
+    }
 }
 
 
 long long int extract_number(char * raw_data){
-    char extracted_hex[9];
+    char extracted_hex[12];
     long long int extracted_dec;
     int length;
-    
+
     length = strlen(raw_data);
     for(int i=3; i<length; i++){
         if(raw_data[i] != ','){
@@ -78,4 +136,48 @@ long long int extract_number(char * raw_data){
     sscanf(extracted_hex, "%X", &extracted_dec);
 
     return extracted_dec;
+}
+
+
+
+bool isEmpty (const struct Window * window){
+	return window->first == NULL;
+}
+
+bool isfull (const struct Window * window, int wsize){
+	return window->amount == wsize;
+}
+
+
+void leaveFront (struct Window * window){
+	assert(!isEmpty(window));
+	struct Node *old_first;
+    old_first = window->first;
+    window->first = old_first->next;
+    free(old_first);
+	if(window->first != NULL){
+		window->first-> prev = NULL;}
+	else{
+		window->last = NULL;}
+
+}
+
+
+
+void addBack (struct Window * window, long long int page_number){
+	struct Node *new_node = malloc(sizeof(struct Node));
+	new_node->page_number = malloc(sizeof(page_number)+1);
+	new_node->page_number = page_number;
+	if(!isEmpty(window)){
+    	new_node->prev = window->last;
+    	window->last->next = new_node;
+    	window->last = new_node;
+		//free(new_node);
+	}else{
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		window->first = new_node;
+		window->last = new_node;
+		//free(new_node);
+	}
 }
