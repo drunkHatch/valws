5ebf378b3e1304fa70ff414bfaaa0b63fd92d5d1
taijie <taijie@ualberta.ca>
2018-04-05 18:53:13 -0600

    Add files via upload

---
+++ lab121.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+
+with open('data.csv','rt') as data:
+    reader = csv.reader(data)
+    value = []
+    for i in reader:
+        #print(i)
+        #print(type(i[0]))
+        data = int(i[0])
+        value.append(data)
+
+    #for j in value:
+        #count = value.count(j)
+        #freq.append(count)
+
+
+    
+ 
+    
+    
+
+x1 = range(1,len(value)+1)
+y1 = value
+
+
+
+plt.plot(x1,y1)
+#plt.hist(value,20)
+
+plt.show()
+
+print("done")
